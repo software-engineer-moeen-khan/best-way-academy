5c61a6e47239494d8062f181c5cffd21ab95c59e
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-08 16:44:58 +0500

    Support MySQL cache store

--- config/cache.php
+++ config/cache.php
@@ -1,3 +1,26 @@
 <?php
+
 use Illuminate\Support\Str;
-return ['default'=>env('CACHE_STORE','file'),'stores'=>['file'=>['driver'=>'file','path'=>storage_path('framework/cache/data'),'lock_path'=>storage_path('framework/cache/data')],'array'=>['driver'=>'array','serialize'=>false]],'prefix'=>env('CACHE_PREFIX',Str::slug((string)env('APP_NAME','laravel')).'-cache-')];
+
+return [
+    'default'=>env('CACHE_STORE','file'),
+    'stores'=>[
+        'database'=>[
+            'driver'=>'database',
+            'connection'=>env('CACHE_DB_CONNECTION'),
+            'table'=>env('CACHE_DB_TABLE','cache'),
+            'lock_connection'=>env('CACHE_DB_LOCK_CONNECTION'),
+            'lock_table'=>env('CACHE_DB_LOCK_TABLE','cache_locks'),
+        ],
+        'file'=>[
+            'driver'=>'file',
+            'path'=>storage_path('framework/cache/data'),
+            'lock_path'=>storage_path('framework/cache/data'),
+        ],
+        'array'=>[
+            'driver'=>'array',
+            'serialize'=>false,
+        ],
+    ],
+    'prefix'=>env('CACHE_PREFIX',Str::slug((string)env('APP_NAME','laravel')).'-cache-'),
+];
