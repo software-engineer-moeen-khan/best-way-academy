28fb5c71120661cdf8754abffc0944435bf2f414
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-08 17:39:12 +0500

    Allow managed user roles and status

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'email', 'password', 'role', 'status'];
     protected $hidden = ['password', 'remember_token'];
 
     protected function casts(): array
