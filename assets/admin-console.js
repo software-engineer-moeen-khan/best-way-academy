e997bd8716b7747cd5a6ca1740efee3d731393cd
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-08 18:00:53 +0500

    Load complete admin modules

--- assets/admin-console.js
+++ assets/admin-console.js
@@ -1,1 +1,2 @@
-document.write('<script src="/assets/admin.js?rev=20260808-admin-management-v1"><\/script>');
+document.write('<script src="/assets/admin.js?rev=20260808-admin-management-v2"><\/script>');
+document.write('<script src="/assets/admin-extras.js?rev=20260808-admin-management-v2"><\/script>');
