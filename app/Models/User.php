5302bbed8dd01999b1c08f47d8865895c65408ed
software-engineer-moeen-khan <121344844+software-engineer-moeen-khan@users.noreply.github.com>
2026-08-08 15:56:43 +0500

    Prevent role mass assignment

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $fillable = ['name', 'email', 'password', 'role'];
+    protected $fillable = ['name', 'email', 'password'];
     protected $hidden = ['password', 'remember_token'];
 
     protected function casts(): array
